fcaf211d0a8e296274cbb9c0d4a34257c406539c
cyj <yjchen0819@gmail.com>
2024-03-25 01:50:44 +0800

    second commit

--- question02/main.py
+++ question02/main.py
@@ -1,12 +1,38 @@
 from workout_a import construct_portfolio
 from workout_a import calculate_return;
-from workout_a import calculate_risk;
+from workout_a import calculate_risk_and_contribution;
 
+title = [    "      [spy-0% / govt-100%]: ", 
+             "      [spy-10% / govt-90%]: ",
+             "      [spy-20% / govt-80%]: ",
+             "      [spy-30% / govt-70%]: ",
+             "      [spy-40% / govt-60%]: ",
+             "      [spy-50% / govt-50%]: ",
+             "      [spy-60% / govt-40%]: ",
+             "      [spy-70% / govt-30%]: ",
+             "      [spy-80% / govt-20%]: ",
+             "      [spy-90% / govt-10%]: ",
+             "      [spy-100% / govt-0%]: "    ]
 
 def solution_02():
-    portfolio_value_pairs, data_list = construct_portfolio(1000000)
-    calculate_return(portfolio_value_pairs)
-    calculate_risk(data_list)
+    number_of_shares, value_pairs, data_list = construct_portfolio(1000000)
+    portfolio_return_list = calculate_return(value_pairs)
+    portfolio_risk_list, risk_contribution_ratio_list = calculate_risk_and_contribution(data_list)
     
+    print('-> no.of shares: ')
+    for index, elem in enumerate(number_of_shares):
+        print(title[index], end = '')
+        print(elem[0], ', ', elem[2], ', ', elem[4], ', ', elem[1], ', ', elem[3], ', ', elem[5], sep = '')
+        
+    print('\n-> annualized portfolio risk and return, with risk contribution ratio: ')
+    for index, elem in enumerate(portfolio_return_list):
+        rtn = elem
+        risk = portfolio_risk_list[index]
+        contribution_ratio = risk_contribution_ratio_list[index]
+        print(title[index], end = '')
+        print('return: ', rtn, '%', '; risk: ', risk, '; return/risk: ', round((rtn / risk), 2), sep = '')
+        spaces = ' ' * (len(title[index]) - 2)
+        print(spaces, '-> ', 'risk contribution ratio of spy: ', contribution_ratio[0], '%, ', 'govt: ', contribution_ratio[1], '%', sep = '')
+        
 if __name__ == "__main__":
     solution_02()
--- question02/workout_a.py
+++ question02/workout_a.py
@@ -14,6 +14,7 @@ class PortfolioValue:
     def __init__(self, portfolio_value):
         self.portofolio_value = portfolio_value
 
+ratio = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 def read_data_from_xl(file_path):
     workbook = load_workbook(file_path)
@@ -69,7 +70,11 @@ def construct_portfolio(init_capital):
     # for return and risk calculation
     portfolio_value_pairs = []
     
-    ratio = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    # from 0%-100% to 100%-0%
+    # store the no. of shares of spy and govt (2021, 2022, 2023)
+    number_of_shares = []
+    
+    # ratio = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
     for r in ratio:
         sheet = output_workbook.create_sheet(str(r * 100) + '%' + '-' + str(100 - r * 100) + '%')
         sheet['A1'] = 'Date'
@@ -84,6 +89,9 @@ def construct_portfolio(init_capital):
     
         shares_spy = init_capital * weight_spy / init_price_spy
         shares_govt = init_capital * weight_govt / init_price_govt
+        shares_list = []
+        shares_list.extend([round(shares_spy, 2), round(shares_govt, 2)])
+        
         value_last = 0.0
     
         for index, data in enumerate(list_1):
@@ -94,6 +102,7 @@ def construct_portfolio(init_capital):
         # re-balancing
         shares_spy = value_last * weight_spy / list_1[-1].price_spy
         shares_govt = value_last * weight_govt / list_1[-1].price_govt
+        shares_list.extend([round(shares_spy, 2), round(shares_govt, 2)])
         for index, data in enumerate(list_2):
             p_val = shares_spy * data.price_spy + shares_govt * data.price_govt
             sheet.append(get_row_data(data, index, shares_spy, shares_govt))
@@ -102,6 +111,7 @@ def construct_portfolio(init_capital):
         # re-balancing
         shares_spy = value_last * weight_spy / list_2[-1].price_spy
         shares_govt = value_last * weight_govt / list_2[-1].price_govt
+        shares_list.extend([round(shares_spy, 2), round(shares_govt, 2)])
         for index, data in enumerate(list_3):
             p_val = shares_spy * data.price_spy + shares_govt * data.price_govt
             sheet.append(get_row_data(data, index, shares_spy, shares_govt))
@@ -110,11 +120,13 @@ def construct_portfolio(init_capital):
         value_end = sheet.cell(row=sheet.max_row, column=4).value
         # print('v1 = ', value_start, 'v2 = ', value_end)
         portfolio_value_pairs.append([value_start, value_end])
+        
+        number_of_shares.append(shares_list)
 
     save_filepath = 'data\daily portfolio value.xlsx'
     output_workbook.save(save_filepath)
     
-    return portfolio_value_pairs, data_list
+    return number_of_shares, portfolio_value_pairs, data_list
 
 def calculate_return(portfolio_value_pairs):
     # the range of year is 3
@@ -130,57 +142,76 @@ def calculate_return(portfolio_value_pairs):
         annualized_rtn = ((1 + rtn_value) ** (1 / n) - 1) * 100
         annualized_rtn = round(annualized_rtn, 2)
         portfolio_returns.append(annualized_rtn)
-        print('return: ', annualized_rtn, '%', sep='')
+        # print('return: ', annualized_rtn, '%', sep='')
+    
+    return portfolio_returns
         
-def calculate_risk(data_list):
-    t = 252
-    share_spy = 0
-    share_govt = 1
-    rtn_arr_spy = []
-    rtn_arr_govt = []
-    pre_price_spy = data_list[0].price_spy
-    pre_price_govt = data_list[0].price_govt
-    for index, data in enumerate(data_list):
-        cur_price_spy = data.price_spy
-        cur_price_govt = data.price_govt
-        if index > 0:
-            rtn_spy = (cur_price_spy / pre_price_spy) - 1
-            rtn_govt = (cur_price_govt / pre_price_govt) - 1
-            rtn_arr_spy.append(rtn_spy)
-            rtn_arr_govt.append(rtn_govt)
-        pre_price_spy = cur_price_spy
-        pre_price_govt = cur_price_govt
+def calculate_risk_and_contribution(data_list):
+    
+    portfolio_risk = []
+    risk_contribution_ratio = []
+    
+    for r in ratio:
+        t = 252
+        share_spy = r
+        share_govt = 1 - r
+        rtn_arr_spy = []
+        rtn_arr_govt = []
+        pre_price_spy = data_list[0].price_spy
+        pre_price_govt = data_list[0].price_govt
+        for index, data in enumerate(data_list):
+            cur_price_spy = data.price_spy
+            cur_price_govt = data.price_govt
+            if index > 0:
+                rtn_spy = (cur_price_spy / pre_price_spy) - 1
+                rtn_govt = (cur_price_govt / pre_price_govt) - 1
+                rtn_arr_spy.append(rtn_spy)
+                rtn_arr_govt.append(rtn_govt)
+            pre_price_spy = cur_price_spy
+            pre_price_govt = cur_price_govt
             
-    rtn_avg_spy = sum(rtn_arr_spy) / len(rtn_arr_spy)
-    rtn_avg_govt = sum(rtn_arr_govt) / len(rtn_arr_govt)
+        rtn_avg_spy = sum(rtn_arr_spy) / len(rtn_arr_spy)
+        rtn_avg_govt = sum(rtn_arr_govt) / len(rtn_arr_govt)
     
-    corr_spy_spy = 0.0
-    corr_govt_govt = 0.0
-    corr_spy_govt = 0.0
+        corr_spy_spy = 0.0
+        corr_govt_govt = 0.0
+        corr_spy_govt = 0.0
     
-    # calculate the correlation of spy
-    for data in rtn_arr_spy:
-        corr_spy_spy += (data - rtn_avg_spy) ** 2
+        # calculate the correlation of spy
+        for data in rtn_arr_spy:
+            corr_spy_spy += (data - rtn_avg_spy) ** 2
     
-    corr_spy_spy = math.sqrt(corr_spy_spy / (len(rtn_arr_spy) - 1))
-    # adjust with annualized factor
-    corr_spy_spy *= math.sqrt(t)
+        corr_spy_spy = math.sqrt(corr_spy_spy / (len(rtn_arr_spy) - 1))
+        # adjust with annualized factor
+        corr_spy_spy *= math.sqrt(t)
     
-    # calculate the correlation of govt
-    for data in rtn_arr_govt:
-        corr_govt_govt += (data - rtn_avg_govt) ** 2
+        # calculate the correlation of govt
+        for data in rtn_arr_govt:
+            corr_govt_govt += (data - rtn_avg_govt) ** 2
     
-    corr_govt_govt = math.sqrt(corr_govt_govt / (len(rtn_arr_govt) - 1))
-    # adjust with annualized factor
-    corr_govt_govt *= math.sqrt(t)
+        corr_govt_govt = math.sqrt(corr_govt_govt / (len(rtn_arr_govt) - 1))
+        # adjust with annualized factor
+        corr_govt_govt *= math.sqrt(t)
     
-    # calculate the correlation of spy and govt
-    for i in range(len(rtn_arr_spy)):
-        corr_spy_govt += (rtn_arr_spy[i] - rtn_avg_spy) * (rtn_arr_govt[i] - rtn_avg_govt)
-    corr_spy_govt *= (t / (len(rtn_arr_spy) - 1))
+        # calculate the correlation of spy and govt
+        for i in range(len(rtn_arr_spy)):
+            corr_spy_govt += (rtn_arr_spy[i] - rtn_avg_spy) * (rtn_arr_govt[i] - rtn_avg_govt)
+        corr_spy_govt *= (t / (len(rtn_arr_spy) - 1))
     
-    corr_coeff = corr_spy_govt / (corr_spy_spy * corr_govt_govt)
+        corr_coeff = corr_spy_govt / (corr_spy_spy * corr_govt_govt)
     
-    protfolio_variance = share_spy ** 2 * corr_spy_spy ** 2 + share_govt ** 2 * corr_govt_govt ** 2 + 2 * corr_coeff * share_spy * share_govt * corr_spy_spy * corr_govt_govt
-    portfolio_risk = math.sqrt(protfolio_variance)
-    print(portfolio_risk)
+        protfolio_variance = share_spy ** 2 * corr_spy_spy ** 2 + share_govt ** 2 * corr_govt_govt ** 2 + 2 * corr_coeff * share_spy * share_govt * corr_spy_spy * corr_govt_govt
+        risk = math.sqrt(protfolio_variance)
+        # print(portfolio_risk)
+        portfolio_risk.append(round(risk  * 100, 2))
+        
+        # calculate risk contribution ratio
+        contribution_spy = share_spy ** 2 * corr_spy_spy ** 2 + share_spy * share_govt * corr_coeff * corr_spy_spy * corr_govt_govt
+        contribution_govt = share_govt ** 2 * corr_govt_govt ** 2 + share_spy * share_govt * corr_coeff * corr_spy_spy * corr_govt_govt
+        contribution_ratio_spy = contribution_spy / protfolio_variance
+        contribution_ratio_govt = contribution_govt / protfolio_variance
+        
+        risk_contribution_ratio.append([round(contribution_ratio_spy * 100, 2), round(contribution_ratio_govt * 100, 2)])
+        
+    return portfolio_risk, risk_contribution_ratio
+        
